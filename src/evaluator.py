85b86ae2778d8512b0343c5de5a93f640f0daed0
Leukas <J.L.Edman@student.rug.nl>
2019-09-04 15:01:53 +0200

    newline char added to bootstrap writing

--- src/evaluator.py
+++ src/evaluator.py
@@ -325,7 +325,7 @@ class EvaluatorMT(object):
         for i in range(1000):
             fake_sample, real_sample = resample(fake_sims.cpu().numpy(), real_sims.cpu().numpy())
             scs[i] = real_sim_score(fake_sample, real_sample)
-            for_pair_file.write(str(scs[i]))
+            for_pair_file.write(str(scs[i]) + '\n')
         
         for_pair_file.close()
         logger.info("Bootstrap-resampling done.")
@@ -418,7 +418,7 @@ class EvaluatorMT(object):
         for i in range(1000):
             fake_sample, real_sample = resample(fake_sims.cpu().numpy(), real_sims.cpu().numpy())
             scs[i] = real_sim_score(fake_sample, real_sample)
-            for_pair_file.write(str(scs[i]))
+            for_pair_file.write(str(scs[i]) + '\n')
         
         for_pair_file.close()
         logger.info("Bootstrap-resampling done.")
